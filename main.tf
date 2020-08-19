eefeff5cc213a3f0a698c0662ad99a5d94f264f4
David Vargas <dvargas92495@gmail.com>
2020-08-19 19:29:29 -0400

    Adding GH Actions

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ resource "aws_route53_record" "cert" {
     name    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
     type    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_type
     records = [tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_value]
-    zone_id = element(data.aws_route53_zone.zone[0].zone_id, 0)
+    zone_id = data.aws_route53_zone.zone[0].zone_id
     ttl     = 300
 }
 
