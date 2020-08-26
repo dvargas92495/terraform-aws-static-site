d3b6decd96c2f062d5fcca8593d9003428d5faa0
David Vargas <dvargas92495@gmail.com>
2020-08-26 15:43:22 -0400

    Cyclic Dependency

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ data "aws_iam_policy_document" "bucket_policy" {
     ]
 
     resources = [
-      "${aws_s3_bucket.main.arn}/*",
+      "arn:aws:s3:::${local.primary_domain}/*",
     ]
 
     condition {
