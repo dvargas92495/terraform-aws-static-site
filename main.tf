6a6be481a928cf4b0a85192898d8f66a0e4dcda9
David Vargas <dvargas92495@gmail.com>
2020-09-18 21:27:44 -0400

    Adding custom error responses to cloudfront

--- main.tf
+++ main.tf
@@ -190,6 +190,18 @@ resource "aws_cloudfront_distribution" "cdn" {
         }
       }
     }
+
+    custom_error_response {
+      error_code = 404
+      response_code = 200
+      response_page_path = "/index.html"
+    }
+
+    custom_error_response {
+      error_code = 403
+      response_code = 200
+      response_page_path = "/index.html"
+    }
 }
 
 resource "aws_route53_record" "A" {
