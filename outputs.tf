dcdae85513a0753f14aa3347515226c8d2a7c062
David Vargas <dvargas92495@gmail.com>
2020-09-24 12:50:12 -0400

    Output R53 zone id

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "bucket-name" {
   description = "The name of the main S3 bucket."
   value       = aws_s3_bucket.main.bucket
 }
+
+output "route53_zone_id" {
+  description = "The zone id of the given route53 domain"
+  value       = data.aws_route53_zone.zone.zone_id
+}
