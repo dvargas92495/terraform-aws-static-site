2aba77125073ec41557d7ebb9df7cf96c90e9109
David Vargas <dvargas92495@gmail.com>
2021-05-23 18:56:33 -0600

    just list

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "domain" {
 }
 
 variable "redirects" {
-    type        = list<string>
+    type        = list
     description = "List of domains that redirect to the main domain"
     default     = []
 }
